f8f47ccfa351dae4d47efc379d38a93ba4c61a65
Jacksonishere <jackson.lu02@myhunter.cuny.edu>
2021-10-27 11:18:07 -0400

    added associations in comment model for nested comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,11 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
+  #nesting comments relationship 
+
+  #convention
+  #knows to go to the comments table and look for a FK called comment_id.
+  has_many :comments, foreign_key: :parent_id
+  #knows to go to the class/model called parent but since there is none, we need to specify we're going back to comment
+  belongs_to :parent, class_name: 'Comment'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_26_152643) do
+ActiveRecord::Schema.define(version: 2021_10_27_141434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_10_26_152643) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "parent_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
